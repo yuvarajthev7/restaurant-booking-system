30299f98a25d11aaa2a4d212f94992f684ca20fc
Yuvaraj <yuvarajthev7@gmail.com>
2025-12-03 10:35:03 +0530

    initial push

---
+++ src/main/java/com/restaurant_booking/model/User.java
@@ -0,0 +1,32 @@
+package com.restaurant_booking.model; 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "app_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
