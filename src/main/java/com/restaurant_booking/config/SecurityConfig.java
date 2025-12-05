822b7a4c2968136196cc764dfbd5807afeedb1c8
Yuvaraj <yuvarajthev7@gmail.com>
2025-12-05 19:58:07 +0530

    deployed on render

--- src/main/java/com/restaurant_booking/config/SecurityConfig.java
+++ src/main/java/com/restaurant_booking/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://restaurant-booking-system-v7hc.onrender.com","http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://restaurant-booking-system-main-frontend.onrender.com/","http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
